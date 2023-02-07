dd46bc19e58a993881187d1ce46ec7de166d6985
Vovchek <39309092+Vovchek@users.noreply.github.com>
2023-02-07 12:52:27 +0300

    in progress

--- alloc_test.cpp
+++ alloc_test.cpp
@@ -2,3 +2,12 @@
 #include "log_alloc.h"
 #include "dumb_container.h"
 
+TEST(AllocTest, Test1)
+{
+// test if container with logging_allocator works 
+}
+
+TEST(AllocTest, Test2)
+{
+// test if memory is freed after container is destroyed
+}
